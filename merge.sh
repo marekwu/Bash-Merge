72cf7dd7e28ad364d0965bb0dcb0cd78f97dff28
Marek Witkowski <marek.witkowski.priv@gmail.com>
2020-02-03 21:40:18 +0100

    file tests added.

--- merge.sh
+++ merge.sh
@@ -9,8 +9,6 @@ function help {
 	echo "template.tex - LaTeX template file."
 }
 
-
-
 if [ $# -ne 2 ]
 then
 	echo "Error - wrong number of arguments\n"
@@ -18,9 +16,21 @@ then
 	exit 1
 fi
 
-
 DATAFILE=$0
 TEMPLATEFILE=$1
+
+if [ ! -f DATAFILE ]
+then
+	echo "Error - data file doesn't exist."
+	exit 1
+fi
+
+if [ ! -f TEMPLATEFILE ]
+then
+	echo "Error - template file does't exist."
+	exit 1
+fi
+
 
 echo "Data file     : $DATAFILE"
 echo "Template file : $TEMPLATEFILE"
