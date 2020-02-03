7ad544b311d831cfb8f3fdb6cf5fdc7c95f12ee1
Marek Witkowski <marek.witkowski.priv@gmail.com>
2020-02-03 22:00:44 +0100

    daily update

--- merge.sh
+++ merge.sh
@@ -16,24 +16,31 @@ then
 	exit 1
 fi
 
-DATAFILE=$0
-TEMPLATEFILE=$1
+DATAFILE=$1
+TEMPLATEFILE=$2
 
-if [ ! -f DATAFILE ]
+if [ -f DATAFILE ]
 then
-	echo "Error - data file doesn't exist."
+	echo "Error - data file $DATAFILE doesn't exist."
 	exit 1
 fi
 
-if [ ! -f TEMPLATEFILE ]
+if [ -f TEMPLATEFILE ]
 then
-	echo "Error - template file does't exist."
+	echo "Error - template file $TEMPLATEFILE does't exist."
 	exit 1
 fi
 
 
 echo "Data file     : $DATAFILE"
 echo "Template file : $TEMPLATEFILE"
+
+IFS=','
+
+while read id first_name last_name email gender Address Code City
+	do
+		echo $first_name
+	done < $DATAFILE
 
 
 
