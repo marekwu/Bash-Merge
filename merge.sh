c6ad5221fded37b89918dd3c285891d3f7109ef5
Marek Witkowski <marek.witkowski.priv@gmail.com>
2020-02-03 13:47:20 +0100

    help function added.

--- merge.sh
+++ merge.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 
+
+function help {
+
+	echo -e "Usage:\n"
+	echo -e "mailmerge.sh data.csv template.tex\n"
+	echo "data.csv - CSV data file."
+	echo "template.tex - LaTeX template file."
+}
+
+
+
+if [ $# -ne 2 ]
+then
+	echo "Error - wrong number of arguments\n"
+	help
+	exit 1
+fi
+
+
+DATAFILE=$0
+TEMPLATEFILE=$1
+
+echo "Data file     : $DATAFILE"
+echo "Template file : $TEMPLATEFILE"
+
+
